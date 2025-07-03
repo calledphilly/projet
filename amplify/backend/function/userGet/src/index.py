307375f683f0b830cb79601927bd95a7157bcac3
calledphilly <ybikouta@icloud.com>
2025-07-03 16:29:38 +0200

    Update AWS Lambda functions to include DynamoDB integration and permissions. Modified userAdd and userGet functions to handle storage permissions and added necessary environment variables. Updated CloudFormation templates to reflect new dependencies and IAM policies. Adjusted function parameters to include permissions for storage operations.

--- amplify/backend/function/userGet/src/index.py
+++ amplify/backend/function/userGet/src/index.py
@@ -1,15 +1,42 @@
 import json
+import os
+import uuid
+import boto3
+
+# dynamodb = boto3.resource("dynamodb")
+# table = dynamodb.Table(os.environ["STORAGE_USERTABLE_NAME"])
+TABLE_NAME = os.environ.get("STORAGE_USERTABLE_NAME", "userTabel-dev")
+dynamodb = boto3.resource("dynamodb", region_name="eu-west-1")
+table = dynamodb.Table(TABLE_NAME)
 
 def handler(event, context):
   print('received event:')
   print(event)
   
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
+  try:
+    email = event.get("email", "{}")
+
+    if not email:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "Missing 'email' in request"})
+        }
+
+    user_id = str(uuid.uuid4())
+    table.put_item(Item={"userId": user_id, "email": email})
+  
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body': json.dumps('Hello from your new Amplify Python lambda!')
+    }
+  except Exception as e:
+        print(f"Error: {e}")
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
